288ff7b4580f98f0f20b670ed91d9adc3e86efe6
tushar <tushar158gupta@gmail.com>
2022-10-01 16:13:41 +0530

    initial commit. Done css from my side

---
+++ components/Card.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+
+import { AiFillInstagram , AiFillTwitterCircle , AiFillLinkedin } from "react-icons/ai";
+
+function Card({ image , name , about  , instagram , twitter , linkedin}) {
+  return (
+    <div className='flex flex-col self-center p-10 max-w-md content-center text-center rounded-lg shadow-lg border-2 border-gray-300 '>
+       <div className="">
+        <Image src={image}  alt="Picture of the author"
+      width={150}
+      height={150} className="rounded-full" />
+       </div>
+       <div>
+        <h2 className='text-3xl font-medium mt-5 text-slate-100'>{name}</h2>
+        <h3 className='text-xl py-5 font-300 text-slate-200 text-ellipsis overflow-hidden'>{about}</h3>
+       </div>
+       <div className='mx-auto flex text-4xl gap-6 text-gray-100 py-3'>
+        <a href={instagram} target="_blank">
+        <AiFillInstagram  className='cursor-pointer hover:text-white hover:scale-125'/>
+        </a>
+        <a href={twitter} target="_blank">
+        <AiFillTwitterCircle className='cursor-pointer hover:text-white hover:scale-125'/>
+        </a>
+        <a href={linkedin} target="_blank">
+        <AiFillLinkedin className='cursor-pointer hover:text-white hover:scale-125'/>
+        </a>
+       </div>
+    </div>
+  )
+}
+
+export default Card
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['tmdb.org', 'themoviedb.org','www.instagram.com'],
+  },
 }
 
 module.exports = nextConfig
