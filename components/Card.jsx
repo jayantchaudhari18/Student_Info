444f2fec5792703198ee298e8201c904f292536f
tushar <tushar158gupta@gmail.com>
2022-10-01 20:47:40 +0530

    done with flex designing

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-import { AiFillInstagram , AiFillTwitterCircle , AiFillLinkedin } from "react-icons/ai";
+import {
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
-function Card({ image , name , about  , instagram , twitter , linkedin}) {
+function Card({ image, name, about, instagram, twitter, linkedin }) {
   return (
-    <div className='flex flex-col self-center p-10 max-w-md content-center text-center rounded-lg shadow-lg border-2 border-gray-300 '>
-       <div className="">
-        <Image src={image}  alt="Picture of the author"
-      width={150}
-      height={150} className="rounded-full" />
-       </div>
-       <div>
-        <h2 className='text-3xl font-medium mt-5 text-slate-100'>{name}</h2>
-        <h3 className='text-xl py-5 font-300 text-slate-200 text-ellipsis overflow-hidden'>{about}</h3>
-       </div>
-       <div className='mx-auto flex text-4xl gap-6 text-gray-100 py-3'>
+    <div id="card" className="flex flex-col self-center  p-10 content-center text-center rounded-lg shadow-lg border-2 border-gray-300 ">
+      <style jsx>{`
+      #card{
+        flex: 1 0 300px;
+        max-width: 450px;
+        overflow: hidden;
+
+      }
+      `}</style>
+      <div className="">
+        <Image
+          src={image}
+          alt="Picture of the author"
+          width={150}
+          height={150}
+          className="rounded-full"
+        />
+      </div>
+      <div>
+        <h2 className="text-3xl font-medium mt-5 text-slate-100">{name}</h2>
+        <h3 className="text-xl py-5 font-300 text-slate-200 text-ellipsis overflow-hidden">
+          {about}
+        </h3>
+      </div>
+      <div className="mx-auto flex text-4xl gap-6 text-gray-100 py-3">
         <a href={instagram} target="_blank" rel="noreferrer">
-        <AiFillInstagram  className='cursor-pointer hover:text-white hover:scale-125'/>
+          <AiFillInstagram className="cursor-pointer hover:text-white hover:scale-125" />
         </a>
         <a href={twitter} target="_blank" rel="noreferrer">
-        <AiFillTwitterCircle className='cursor-pointer hover:text-white hover:scale-125'/>
+          <AiFillTwitterCircle className="cursor-pointer hover:text-white hover:scale-125" />
         </a>
         <a href={linkedin} target="_blank" rel="noreferrer">
-        <AiFillLinkedin className='cursor-pointer hover:text-white hover:scale-125'/>
+          <AiFillLinkedin className="cursor-pointer hover:text-white hover:scale-125" />
         </a>
-       </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -21,22 +21,26 @@ export default function Home() {
 
       <main className=' flex gap-8 justify-center flex-wrap min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-10 md:p-5'>
        
-       
+      
 
         <Card image={TusharGupta}name = "Tushar Gupta" about="jai shree ram. keep chilling " instagram= "https://www.instagram.com/trust_afk/"  twitter="https://twitter.com/ohh_hii_tusharr" linkedin="https://www.linkedin.com/in/tushar-gupta-10a151202/"/>
       
-        <Card image={AdityaSharma}name = "Aditya Sharma" about="future billionaire pr abhi gareeb hu." instagram= "https://www.instagram.com/iaddy29/"  twitter="https://twitter.com/iaddy29" linkedin="https://www.linkedin.com/in/aditya-sharma-2b4a64222/"/>
+       
+        <Card image={AdityaSharma}name = "Aditya Sharma" about="future billionaire abhi gareeb hu." instagram= "https://www.instagram.com/iaddy29/"  twitter="https://twitter.com/iaddy29" linkedin="https://www.linkedin.com/in/aditya-sharma-2b4a64222/"/>
        
 
        <Card image={SudhanshuSharma}name = "Sudhanshu Sharma" about="dm for fun" instagram= "https://www.instagram.com/cpt_n3m0_22/"  twitter="https://twitter.com/Sudhanshu226922" linkedin="https://www.linkedin.com/in/sudhanshu-sharma-9376931b7/"/>
-
         
-        <Card image={AnkushMaji}name = "Ankush Maji" about="First Hacktoberfest" instagram="https://www.instagram.com/101ankush/" twitter="" linkedin="https://www.linkedin.com/in/ankush-maji-11555622a/"/>     
+       
+        <Card image={AnkushMaji}name = "Ankush Maji" about="First Hacktoberfest" instagram="https://www.instagram.com/101ankush/" twitter="" linkedin="https://www.linkedin.com/in/ankush-maji-11555622a/"/>
+
+       
         <Card image={AgneyaPathare}name = "Agneya Pathare" about="Digital Creator" instagram= "https://www.instagram.com/agneya_pathare/"  twitter="https://twitter.com/agneya_pathare" linkedin="https://www.linkedin.com/in/agneya-pathare"/>
           
+       
          <Card image={RitikSharma}name = "Ritik Sharma" about="Life is a Myth" instagram= "https://www.instagram.com/i_am_ritik_sharma_/"  twitter="https://twitter.com/RitikSh85448549" linkedin="https://www.linkedin.com/in/ritik-sharma-aa0282224/"/>
  
-
+       
         <Card image={PriyanshuSingh}name = "Priyanshu Singh" about="Amaterasu!" instagram= "https://www.instagram.com/akapriyanshu.dev/"  twitter="https://twitter.com/akapriyanshudev" linkedin="https://www.linkedin.com/in/akapriyanshudev"/>
 
 
